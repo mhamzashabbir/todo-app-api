3101c48a3ede7bbc25376b8de1228390217eda68
Muhammad Hamza <mhamzashabbirs@gmail.com>
2024-01-16 18:58:49 +0500

    router-and-controllers

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+const homeRoutes = require('./routes/home');
+
+dotenv.config();
+port = process.env.PORT || 3000;
 
 const app = express();
 
-mongoose.connect('mongodb+srv://todo:todoApp1@cluster0.0uamrtj.mongodb.net/tasks?retryWrites=true&w=majority')
+app.use(express.json());
+app.use(cors());
+app.use(homeRoutes);
+
+mongoose.connect(process.env.MONGODB_URI)
     .then(result => {
-        console.log('Connected!');
-        app.listen(8080);
+        console.log('Connected!, Listening on port: ' + port + '...');
+        app.listen(port);
     })
     .catch(err => console.log(err));
